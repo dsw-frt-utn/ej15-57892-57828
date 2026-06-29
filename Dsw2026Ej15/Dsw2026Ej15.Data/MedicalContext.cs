1ee8804fe2d58268cbd52494ff92313e32de8c1f
Valentina Luna <lourdes.lunaacosta@alu.frt.utn.edu.ar>
2026-06-29 16:52:29 -0300

    Cambios en la rama realizados

---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/MedicalContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Dsw2026Ej15.Domain.Entities;
+
+
+namespace Dsw2026Ej15.Data;
+public class MedicalContext : DbContext
+{
+    public MedicalContext(DbContextOptions<MedicalContext> options) : base(options)
+    {
+    }
+
+    // Mapeamos las entidades a tablas de la base de datos
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Speciality> Specialities { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configuramos la tabla Specialities
+        modelBuilder.Entity<Speciality>(entity =>
+        {
+            entity.HasKey(s => s.Id);
+            entity.Property(s => s.Name).IsRequired().HasMaxLength(100);
+        });
+
+        // Configuramos la tabla Doctors
+        modelBuilder.Entity<Doctor>(entity =>
+        {
+            entity.HasKey(d => d.Id);
+            entity.Property(d => d.Name).IsRequired().HasMaxLength(100);
+            entity.Property(d => d.LicenseNumber).IsRequired().HasMaxLength(50);
+            entity.Property(d => d.IsActive).HasDefaultValue(true);
+
+            // Relación: Un médico tiene una especialidad (1 a muchos)
+            entity.HasOne(d => d.Speciality)
+                  .WithMany()
+                  .HasForeignKey("SpecialityId") // EF creará esta columna como clave foránea en las sombras
+                  .IsRequired();
+        });
+    }
+}
---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/PersistenceEf.cs
@@ -0,0 +1,46 @@
+﻿using Dsw2026Ej15.Domain.Entities;
+using Dsw2026Ej15.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dsw2026Ej15.Data;
+
+public class PersistenceEf : IPersistence
+{
+    private readonly MedicalContext _context;
+
+    public PersistenceEf(MedicalContext context)
+    {
+        _context = context;
+    }
+
+    public void AddDoctor(Doctor doctor)
+    {
+        _context.Doctors.Add(doctor);
+        _context.SaveChanges();
+    }
+
+    public Doctor? GetDoctorById(Guid id)
+    {
+        return _context.Doctors
+            .Include(d => d.Speciality)
+            .FirstOrDefault(d => d.Id == id);
+    }
+
+    public IEnumerable<Doctor> GetDoctors()
+    {
+        return _context.Doctors
+            .Include(d => d.Speciality)
+            .ToList();
+    }
+
+    public Speciality? GetSpecialityById(Guid id)
+    {
+        return _context.Specialities.FirstOrDefault(s => s.Id == id);
+    }
+
+    public IEnumerable<Speciality> GetSpecialities()
+    {
+        return _context.Specialities.ToList();
+    }
+}
+
